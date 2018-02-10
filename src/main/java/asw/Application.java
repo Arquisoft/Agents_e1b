fdf7ec981ef404cf6db76c9c2a1d379aa1949b41
Esteban Montes <sankoxh@live.com>
2018-02-10 22:11:56 +0100

    Modificamos datos de prueba de <<Application.java>> y comentadas las pruebas desde <<T0, TN>> para comprobar que tambien lo puede compilar travys

--- src/main/java/asw/Application.java
+++ src/main/java/asw/Application.java
@@ -27,48 +27,14 @@ public class Application {
 
 	@Bean
 	public CommandLineRunner initDB(AgentRepository repository) throws ParseException {
-		// Formato para la fecha
-		DateFormat formatter1 = new SimpleDateFormat("mm/DD/yyyy");
 
 		return (args) -> {
-			// Obtención de fecha
-			Date fecha = (Date) formatter1.parse("08/16/1970");
-			// Inserción en la base de datos
-			repository.save(new Agent("Paco", "Gómez", "123456", fecha, "paco@hotmail.com", "12345678A",
-					"Calle Uría", "Española", false, false));
-
-			// Obtención de fecha
-			Date fecha2 = (Date) formatter1.parse("02/23/1975");
-			// Inserción en la base de datos
-			repository.save(new Agent("Pepe", "Fernández", "123456", fecha2, "pepe@gmail.com", "87654321B",
-					"Calle Principal", "Española", false, false));
-
-			// Obtención de fecha
-			Date fecha3 = (Date) formatter1.parse("08/16/1970");
-			// Inserción en la base de datos
-			repository.save(new Agent("Carmen", "López", "123456", fecha3, "carmen@yahoo.com", "11223344C",
-					"Calle Calvo Sotelo", "Española", false, false));
-
-			// Obtención de fecha
-			Date fecha4 = (Date) formatter1.parse("02/23/1975");
-			// Inserción en la base de datos
-			repository.save(new Agent("Isabel", "Rodríguez", "123456", fecha4, "isabel@gmail.com", "22334455D",
-					"Avenida Galicia", "Española", false, false));
-
-			// ADMIN
-			// Obtención de fecha
-			Date fecha5 = (Date) formatter1.parse("02/23/1975");
-			// Inserción en la base de datos
-			repository.save(new Agent("María", "Sánchez", "123456", fecha5, "maria@gmail.com", "33445566E",
-					"Avenida Santander", "Española", true, false));
-
-			// POLITICO
-			// Obtención de fecha
-			Date fecha6 = (Date) formatter1.parse("02/23/1975");
-			// Inserción en la base de datos
-			repository.save(new Agent("Jose", "Ballesteros", "123456", fecha6, "jose@gmail.com", "44556677F",
-					"Calle Asturias", "Española", false, true));
-
+			repository.save(new Agent("Paco Gómez", "123456", "paco@hotmail.com", "12345678A", "Calle Uría", "Person", 1));
+			repository.save(new Agent("Pepe Fernández", "123456", "pepe@gmail.com", "87654321B", "Calle Principal", "Person", 1));
+			repository.save(new Agent("Carmen López", "123456", "carmen@yahoo.com", "11223344C", "Calle Calvo Sotelo", "Person", 1));
+			repository.save(new Agent("Isabel Rodríguez", "123456", "isabel@gmail.com", "22334455D","Avenida Galicia", "Person", 1));
+			repository.save(new Agent("María Sánchez", "123456", "maria@gmail.com", "33445566E","Avenida Santander", "Person", 1));
+			repository.save(new Agent("Jose Ballesteros", "123456", "jose@gmail.com", "44556677F", "Calle Asturias", "Person", 1));
 		};
 	}
 }
